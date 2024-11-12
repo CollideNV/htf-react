8719f41d3b2be12d9e1d62acc1169e9cde85455c
glenn-collide <glenn.hahn@collide.be>
2024-11-12 17:03:07 +0100

    - remove year 2023

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'htf-2023-component';
+import 'htf-component';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -4,6 +4,6 @@ type CustomElement<T> = Partial<T & DOMAttributes<T> & { children: any }>;
 
 namespace JSX {
   interface IntrinsicElements {
-    ["htf-2023"]: CustomElement<any>;
+    ["htf"]: CustomElement<any>;
   }
 }
